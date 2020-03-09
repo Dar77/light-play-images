4cb37a6ff33ea83564f1f7c7fe4c14d99eff9a1f
Darren <darrenware33@gmail.com>
2020-03-09 15:34:28 +1100

    adjust dependencies

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-   const { createNodeField } = boundActionCreators
+exports.onCreateNode = ({ node, getNode, actions }) => {
+   const { createNodeField } = actions
    if(node.internal.type==='SitePage'){
     createNodeField({
         node,
@@ -8,24 +8,6 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     });
    }
 }
-//generating dynamic menus ref: https://spectrum.chat/gatsby-js/general/how-to-dynamically-generate-navigation-based-on-js-pages~4b0b8de2-13f9-4b26-bda2-1553d1990f0c
-/*
-{
-  allSitePage {
-    edges {
-      node {
-        path
-        fields {
-          slug
-        }
-      }
-    }
-  }
-}
-*/
-
-
-
 
 //function to dynamically create gallery pages from the data on Contentful.
 const Promise = require('bluebird')
@@ -91,95 +73,3 @@ exports.createPages = ({ graphql, actions }) => {
     )
   })
 }
-
-//from scooby blog
-/*
-const Promise = require('bluebird')
-const path = require('path')
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
-              }
-            }
-          }
-          `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
-
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach((post, index) => {
-          createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug
-            },
-          })
-        })
-      })
-    )
-  })
-}
-*/
-
-//original code
-/*
-const Promise = require('bluebird')
-const path = require('path')
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
-              }
-            }
-          }
-          `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
-
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach((post, index) => {
-          createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug
-            },
-          })
-        })
-      })
-    )
-  })
-}
-*/
