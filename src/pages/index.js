b3f0661fbd61834ab08dcee430b9d80216502cd7
Darren <darrenware33@gmail.com>
2020-04-18 19:40:47 +1000

    removed no-index meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,6 @@ class RootIndex extends React.Component {
           <Helmet>
             <title>{siteTitle}| Original Artwork & Photography| Image Gallery</title>
             <meta name="description" content={homeData.metaDescription.metaDescription} />
-            <meta name="robots" content="noindex" />
             <link rel="manifest" crossorigin="use-credentials" href="../../manifest.webmanifest" />
             <meta name="theme-color" content="#000" />
             <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic&display=swap" rel="stylesheet"/>
--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -46,7 +46,6 @@ class GalleryTemplate extends React.Component {
           <Helmet>
             <title>{`${gallery.galleryTitle}| Gallery| ${siteTitle}`}</title>
             <meta name="description" content={gallery.metaDescription.metaDescription} />
-            <meta name="robots" content="noindex" />
             <link rel="manifest" crossorigin="use-credentials" href="../../manifest.webmanifest" />
             <meta name="theme-color" content="#000" />
             <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic&display=swap" rel="stylesheet"/>
