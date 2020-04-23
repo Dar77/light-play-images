18a98b2a82d2dfbc61bba9389ac64232068dae1d
Darren <darrenware33@gmail.com>
2020-04-24 09:35:30 +1000

    Adjust site-map options to exclude 'search-by-tag' pages from sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
       resolve: 'gatsby-plugin-sitemap',
       options: {
         exclude: ['/search-by-tag/*'],
+      },
     },
     {
       resolve: 'gatsby-source-contentful',
